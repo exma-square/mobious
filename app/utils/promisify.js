cb0a438b198bdeedc59a3901624254941858e0f8
clonn <clonncd@gmail.com>
2015-05-12 14:41:08 +0800

    remove files

--- app/utils/promisify.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-export default (fn) => new Promise((resolve) => fn((result) => resolve(result)));
--- tests.webpack.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Browser ES6 Polyfill
-require('babel/polyfill');
-
-var context = require.context('./test/spec', true, /\.test\.jsx$|\.test\.js$/);
-context.keys().forEach(context);
