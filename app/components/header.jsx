395487db003c1e782663e3cf22fb269560d76697
tz5514 <tz5514@gmail.com>
2015-07-16 17:10:53 +0800

    require bootstrap

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,10 +5,14 @@ import {IntlMixin} from 'react-intl';
 import imageResolver from 'utils/image-resolver';
 import Spinner from 'components/shared/spinner';
 import LangPicker from 'components/shared/lang-picker';
+import Button from 'react-bootstrap/lib/Button';
+
 
 // Load styles for the header
 // and load the `react-logo.png` image
 // for the `<img src='' />` element
+require('styles/vendor/bootstrap.scss');
+
 let reactLogo;
 if (process.env.BROWSER) {
   require('styles/header.scss');
@@ -64,6 +68,12 @@ class Header extends Component {
           </li>
         </ul>
         <hr />
+          <Button>Default</Button>
+          <Button bsStyle='primary'>Primary</Button>
+          <Button bsStyle='success'>Success</Button>
+          <Button bsStyle='info'>Info</Button>
+          <Button bsStyle='warning'>Warning</Button>
+          <Button bsStyle='danger'>Danger</Button><Button bsStyle='link'>Link</Button>
       </header>
     );
   }
