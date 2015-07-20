90e9bfa02f5b61989ddef5ed000d2d081358b562
tz5514 <tz5514@gmail.com>
2015-07-20 22:20:56 +0800

    調整header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -5,6 +5,7 @@ import {IntlMixin} from 'react-intl';
 import imageResolver from 'utils/image-resolver';
 import Spinner from 'components/shared/spinner';
 import LangPicker from 'components/shared/lang-picker';
+import Img from 'components/shared/img';
 
 import {Navbar, Nav} from 'react-bootstrap';
 // Load styles for the header
@@ -13,7 +14,7 @@ import {Navbar, Nav} from 'react-bootstrap';
 let reactLogo;
 if (process.env.BROWSER) {
   require('styles/application.scss');
-  // require('styles/header.scss');
+  require('styles/header.scss');
   reactLogo = require('images/react-logo.png');
 }
 else {
@@ -30,8 +31,11 @@ class Header extends Component {
 
   render() {
     return (
-      <header className='app--header'>
-        <Navbar brand='Mobius'>
+      <header className='app-header'>
+        <Navbar brand={<Link to='app' className='app-logo'>
+                         <Img src={reactLogo} alt='react-logo' width={75} height={75}/>
+                         <span>mobious</span>
+                       </Link>}>
           <Nav>
             <li>
               <Link to='app'>
@@ -59,42 +63,11 @@ class Header extends Component {
               </Link>
             </li>
           </Nav>
+          <LangPicker
+            store={this.props.flux.getStore('locale')}
+            actions={this.props.flux.getActions('locale')}/>
         </Navbar>
         <Spinner store={this.props.flux.getStore('requests')} />
-        <LangPicker
-          store={this.props.flux.getStore('locale')}
-          actions={this.props.flux.getActions('locale')} />
-        <Link to='app' className='app--logo'>
-          <img src={reactLogo} alt='react-logo' />
-        </Link>
-        <ul className='app--navbar un-select'>
-          <li>
-            <Link to='app'>
-              {this._getIntlMessage('header.users')}
-            </Link>
-          </li>
-          <li>
-            <Link to='guides'>
-              {this._getIntlMessage('header.guides')}
-            </Link>
-          </li>
-          <li>
-            <Link to='protected'>
-              {this._getIntlMessage('header.protected')}
-            </Link>
-          </li>
-          <li>
-            <Link to='beanList'>
-              {this._getIntlMessage('beanManager.title')}
-            </Link>
-          </li>
-          <li>
-            <Link to='postList'>
-              {this._getIntlMessage('postManager.title')}
-            </Link>
-          </li>
-        </ul>
-        <hr />
       </header>
     );
   }
