1814ea992263865c3f7a6d45af7305dae2938115
Ian_Hung <azure820529@gmail.com>
2015-07-26 03:19:29 +0800

    server comment create api and spec

--- test/server/controllers/comment.spec.js
+++ test/server/controllers/comment.spec.js
@@ -7,7 +7,7 @@ describe.only('comment', () => {
       'content': 'test index content'
     }
 
-    let createdComment = await models.Comment.create(testComment);
+    // let createdComment = await models.Comment.create(testComment);
     done();
   });
 
@@ -22,7 +22,6 @@ describe.only('comment', () => {
       });
     });
     let Comments = indexCommentResult;
-    // console.log(Comments[0]);
 
     try {
       Comments.should.be.Array;
@@ -32,5 +31,57 @@ describe.only('comment', () => {
     } catch (e) {
       done(e);
     }
+  });
+
+  it("index all comments", async (done) => {
+
+    let indexCommentResult = await new Promise((resolve, reject) => {
+      request.get("/rest/comment/")
+      .expect(200)
+      .end((error, res) => {
+        if (error) return reject(error);
+        return resolve(res.body.comments);
+      });
+    });
+    let Comments = indexCommentResult;
+
+    try {
+      Comments.should.be.Array;
+      Comments[0].id.should.be.greaterThan(0);
+      Comments[0].should.have.contain.keys('id', 'author', 'content');
+      done();
+    } catch (e) {
+      done(e);
+    }
+  });
+
+  it("create an comment", async (done) => {
+
+    let seedComment = {
+      'author' : 'seedAuthor',
+      'content': 'seedContent'
+    };
+
+    let createdCommentResult = await new Promise((resolve, reject) => {
+      request.post("/rest/comment/")
+      .send(seedComment)
+      .expect(200)
+      .end((error, res) => {
+        if (error) return reject(error);
+        return resolve(res.body.comment);
+      });
+    });
+
+    let result = await createdCommentResult;
+    console.log('result', result);
+    try {
+      result.should.be.Object;
+      result.id.should.be.greaterThan(0);
+      result.author.should.equal(seedComment.author);
+      done();
+    } catch (e) {
+      done(e);
+    }
+
   });
 });
