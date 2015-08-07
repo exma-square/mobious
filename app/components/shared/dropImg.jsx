ed2146b45febecba31083febfd8e83c66b2c2f7e
Ian_Hung <azure820529@gmail.com>
2015-08-07 19:50:01 +0800

    change non-image view to app/images

--- app/components/shared/dropImg.jsx
+++ app/components/shared/dropImg.jsx
@@ -1,26 +1,30 @@
-/*eslint-disable*/
 import React, {Component, PropTypes} from 'react';
 import Dropzone from 'react-dropzone';
 
+let nonImage;
+
+if (process.env.BROWSER) {
+  nonImage = require('images/no-preview.png');
+}
+
 class DropImg extends Component {
 
-  static propTypes: {
-    apiUrl: React.PropTypes.string,
-    flux: React.PropTypes.object,
-    preview: React.PropTypes.object
+  static propTypes = {
+    apiUrl: PropTypes.string.isRequired,
+    flux: PropTypes.object.isRequired,
+    preview: PropTypes.object
   }
-
-
 
   onDrop(files) {
     this.props.flux.getActions('posts').uploadImg(this.props.apiUrl, files, this.props.preview);
   }
 
   render() {
-    let img = '/assets/images/post/no-preview.png';
-    if(this.props.preview.img === undefined || this.props.preview.img === null) {
-      img = '/assets/images/post/no-preview.png';
-    } else {
+    let img = '';
+    if (this.props.preview.img === undefined || this.props.preview.img === null) {
+      img = nonImage;
+    }
+    else {
       img = '/assets/images/post/' + this.props.preview.img;
     }
     return (
